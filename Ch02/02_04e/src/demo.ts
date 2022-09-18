8911072f86a536d9353f5e6c68596bdbaae32f8d
pawelwrzesien <pawel.wrzesien@icloud.com>
2022-09-18 16:10:02 +0200

    Ch02_02_04e - defining enumerable types (enum)

---
+++ Ch02/02_04e/src/demo.ts
@@ -0,0 +1,36 @@
+interface Contact extends Address {
+    id: number;
+    name: ContactName;
+    birthDate?: Date; // ? -> makes it optional
+    status: ContactStatus;
+}
+
+enum ContactStatus {
+    Active = "active",
+    Inactive = 'inactive',
+    New = "new"
+
+}
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
+
+
+let primaryContact: Contact = {
+    // birthDate: new Date("01-01-1980"),
+    id: 12345,
+    name: "John Doe",
+    line1: "John St.",
+    line2: " ",
+    province: "John's province",
+    region: "John's region",
+    postalCode: "John's postal code",
+    status: ContactStatus.Active,
+}
+
+type ContactName = string
